010aaa477841f26544fce1ab1c40e24bc41016d8
Irene van der Blij <irene.van.der.blij@navara.nl>
2022-05-05 17:46:04 +0200

    refactor test to use it.each

--- src/marsrover/rover.spec.ts
+++ src/marsrover/rover.spec.ts
@@ -1,63 +1,42 @@
 import { go, Rover } from './rover';
 
 describe('marsrover', () => {
-  it('should move forward when direction is south', () => {
-    const rover: Rover = { position: [2, 2], direction: 'S' };
-    expect(go(rover, 'F').position).toEqual([2, 1]);
-  });
+  it.each`
+    rover                                   | result
+    ${{ position: [2, 2], direction: 'S' }} | ${[2, 1]}
+    ${{ position: [2, 2], direction: 'N' }} | ${[2, 3]}
+    ${{ position: [2, 2], direction: 'W' }} | ${[1, 2]}
+    ${{ position: [2, 2], direction: 'E' }} | ${[3, 2]}
+  `(
+    'should move to $result when moving forward with rover: $rover',
+    ({ rover, result }: { rover: Rover; result: [] }) => {
+      expect(go(rover, 'F').position).toEqual(result);
+    }
+  );
 
-  it('should move forward when direction is north', () => {
-    const rover: Rover = { position: [2, 2], direction: 'N' };
-    expect(go(rover, 'F').position).toEqual([2, 3]);
-  });
+  it.each`
+    rover                                   | result
+    ${{ position: [2, 2], direction: 'S' }} | ${[2, 3]}
+    ${{ position: [2, 2], direction: 'N' }} | ${[2, 1]}
+    ${{ position: [2, 2], direction: 'W' }} | ${[3, 2]}
+    ${{ position: [2, 2], direction: 'E' }} | ${[1, 2]}
+  `(
+    'should move to $result when moving backward with rover: $rover',
+    ({ rover, result }: { rover: Rover; result: [] }) => {
+      expect(go(rover, 'B').position).toEqual(result);
+    }
+  );
 
-  it('should move forward when direction is west', () => {
-    const rover: Rover = { position: [2, 2], direction: 'W' };
-    expect(go(rover, 'F').position).toEqual([1, 2]);
-  });
-
-  it('should move forward when direction is east', () => {
-    const rover: Rover = { position: [2, 2], direction: 'E' };
-    expect(go(rover, 'F').position).toEqual([3, 2]);
-  });
-
-  it('should move backwards when direction is north', () => {
-    const rover: Rover = { position: [2, 2], direction: 'N' };
-    expect(go(rover, 'B').position).toEqual([2, 1]);
-  });
-
-  it('should move backwards when direction is east', () => {
-    const rover: Rover = { position: [2, 2], direction: 'E' };
-    expect(go(rover, 'B').position).toEqual([1, 2]);
-  });
-
-  it('should move backwards when direction is south', () => {
-    const rover: Rover = { position: [2, 2], direction: 'S' };
-    expect(go(rover, 'B').position).toEqual([2, 3]);
-  });
-
-  it('should move backwards when direction is west', () => {
-    const rover: Rover = { position: [2, 2], direction: 'W' };
-    expect(go(rover, 'B').position).toEqual([3, 2]);
-  });
-
-  it('should turn the rover left when facing north', () => {
-    const rover: Rover = { position: [2, 2], direction: 'N' };
-    expect(go(rover, 'L').direction).toEqual('W');
-  });
-
-  it('should turn the rover left when facing east', () => {
-    const rover: Rover = { position: [2, 2], direction: 'E' };
-    expect(go(rover, 'L').direction).toEqual('N');
-  });
-
-  it('should turn the rover left when facing south', () => {
-    const rover: Rover = { position: [2, 2], direction: 'S' };
-    expect(go(rover, 'L').direction).toEqual('E');
-  });
-
-  it('should turn the rover left when facing west', () => {
-    const rover: Rover = { position: [2, 2], direction: 'W' };
-    expect(go(rover, 'L').direction).toEqual('S');
-  });
+  it.each`
+    rover                                   | result
+    ${{ position: [2, 2], direction: 'S' }} | ${'E'}
+    ${{ position: [2, 2], direction: 'N' }} | ${'W'}
+    ${{ position: [2, 2], direction: 'W' }} | ${'S'}
+    ${{ position: [2, 2], direction: 'E' }} | ${'N'}
+  `(
+    'should turn to $result when moving forward with rover: $rover',
+    ({ rover, result }: { rover: Rover; result: [] }) => {
+      expect(go(rover, 'L').direction).toEqual(result);
+    }
+  );
 });
