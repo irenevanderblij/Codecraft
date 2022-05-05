eb87994385d179fecab5e5fb02fbe76456e989d9
Irene van der Blij <irene.van.der.blij@navara.nl>
2022-05-05 16:31:10 +0200

    refactor turning left

--- src/marsrover/rover.ts
+++ src/marsrover/rover.ts
@@ -1,41 +1,35 @@
 export function go(rover: Rover, instructions: string) {
   let position = rover.position;
-  let direction = rover.direction;
 
-  switch (rover.direction) {
-    case 'N':
-      if (instructions === 'L') {
-        direction = 'W';
-      } else {
+  if (instructions === 'L') {
+    rover = left(rover);
+  } else {
+    switch (rover.direction) {
+      case 'N':
         instructions === 'F' ? position[1]++ : position[1]--;
-      }
-      break;
-    case 'E':
-      if (instructions === 'L') {
-        direction = 'N';
-      } else {
+        break;
+      case 'E':
         instructions === 'F' ? position[0]++ : position[0]--;
-      }
-      break;
-    case 'S':
-      if (instructions === 'L') {
-        direction = 'E';
-      } else {
+        break;
+      case 'S':
         instructions === 'F' ? position[1]-- : position[1]++;
-      }
-      break;
-    case 'W':
-      if (instructions === 'L') {
-        direction = 'S';
-      } else {
+        break;
+      case 'W':
         instructions === 'F' ? position[0]-- : position[0]++;
-      }
-      break;
-    default:
-      break;
+        break;
+      default:
+        break;
+    }
   }
 
-  return { ...rover, position, direction };
+  return { ...rover, position };
+}
+
+function left(rover: Rover): Rover {
+  const directions = ['N', 'E', 'S', 'W'];
+  const newindex =
+    (directions.indexOf(rover.direction) - 1 + directions.length) % directions.length;
+  return { ...rover, direction: directions[newindex] };
 }
 
 export interface Rover {
